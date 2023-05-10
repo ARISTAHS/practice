ddb0fbd2417298cc8e24b77fea885f6423bf91a0
JHS <118969653+ARISTAHS@users.noreply.github.com>
2023-05-10 19:14:47 +0900

    [practice] project v1.7
    html 이미지 태그 추가
    css 호버 효과 추가
    js 비쥬얼 핸들러 스크립트 작성 (미완)

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// visual 이미지 컨트롤 스크립트
+
+const img1 = document.querySelector("#visual ul li:nth-of-type(1) img");
+const img2 = document.querySelector("#visual ul li:nth-of-type(2) img");
+const img3 = document.querySelector("#visual ul li:nth-of-type(3) img");
+const playBtn = document.querySelector("#visual .page_number .play");
+const stopBtn = document.querySelector("visual .page_number .stop");
+
+function handle(e){
+  handleClick: () => {
+    
+  }
+}
+
+playBtn.addEventListener("click",handle.handleClick);
