6d6719c8a39e235745fb9622d3be707bc8c32282
JHS <118969653+ARISTAHS@users.noreply.github.com>
2023-05-15 01:54:25 +0900

    [practice] project v1.7.3
    스크립트 추가

--- js/main.js
+++ js/main.js
@@ -12,7 +12,38 @@ const next = visual.querySelector(".contral .next");
 
 
 const totalImage = imgs.length; // 이미지 전체 갯수
-let currentImage = 0; // 현재 이미지 
+let currentIndex = 0; // 현재 이미지 
 let intervalId;  // setinterval ID 초기값 설정
 
 
+// 이미지 텍스트 변경 함수 
+function changeVisual(){
+  imgs[currentIndex].classList.remove("show");
+  text[currentIndex].classList.remove("show");
+
+  currentIndex ++;
+  if(currentIndex >= totalImage){
+    currentIndex = 0;
+  }
+  imgs[currentIndex].classList.add("show");
+  text[currentIndex].classList.add("show");
+
+  PageNumber();  // 페이지 넘버 버튼 상태 변경
+}
+
+// 이전 이미지로 이동
+function prevImg() {
+  currentIndex--;
+  if (currentIndex < 0) {
+    currentIndex = totalImage - 1;
+  }
+  changeVisual();
+}
+// 다음 이미지로 이동
+function nextImg() {
+  currentIndex++;
+  if (currentIndex >= totalImage) {
+    currentIndex = 0;
+  }
+  changeVisual();
+}
