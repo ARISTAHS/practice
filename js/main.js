8abd398e711965a3acb7f7f1ff0370b16e6c6e57
JHS <118969653+ARISTAHS@users.noreply.github.com>
2023-05-15 01:44:52 +0900

    [practice] project v1.7.2
    css 경로 변경,
    visual html 완 ,
    스크립트 작성 시작 (미완)

--- js/main.js
+++ js/main.js
@@ -1,15 +1,18 @@
-// visual 이미지 컨트롤 스크립트
+// 메인 이미지 컨트롤 스크립트
+const visual = document.querySelector("#visual");  //visual 선언 
 
-const img1 = document.querySelector("#visual ul li:nth-of-type(1) img");
-const img2 = document.querySelector("#visual ul li:nth-of-type(2) img");
-const img3 = document.querySelector("#visual ul li:nth-of-type(3) img");
-const playBtn = document.querySelector("#visual .page_number .play");
-const stopBtn = document.querySelector("visual .page_number .stop");
+// 선언한 visual 내에서 태그 찾기
+const imgs = visual.querySelectorAll("img");    
+const text = visual.querySelectorAll("span");
+const playBtn = visual.querySelector(".page_number .play");
+const stopBtn = visual.querySelector(".page_number .stop");
+const buttons = visual.querySelectorAll(".page_number span.btn");
+const pre = visual.querySelector(".contral .pre");
+const next = visual.querySelector(".contral .next");
 
-function handle(e){
-  handleClick: () => {
-    
-  }
-}
 
-playBtn.addEventListener("click",handle.handleClick);
+const totalImage = imgs.length; // 이미지 전체 갯수
+let currentImage = 0; // 현재 이미지 
+let intervalId;  // setinterval ID 초기값 설정
+
+
