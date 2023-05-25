007388f74bc8c91bec3eed999b3cd39b883ff736
JHS <118969653+ARISTAHS@users.noreply.github.com>
2023-05-26 00:49:43 +0900

    [practice] project v1.7.4

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ const visual = document.querySelector("#visual");  //visual 선언
 
 // 선언한 visual 내에서 태그 찾기
 const imgs = visual.querySelectorAll("img");    
-const text = visual.querySelectorAll("span");
+// const text = visual.querySelectorAll("span");
 const playBtn = visual.querySelector(".page_number .play");
 const stopBtn = visual.querySelector(".page_number .stop");
 const buttons = visual.querySelectorAll(".page_number span.btn");
@@ -19,14 +19,14 @@ let intervalId;  // setinterval ID 초기값 설정
 // 이미지 텍스트 변경 함수 
 function changeVisual(){
   imgs[currentIndex].classList.remove("show");
-  text[currentIndex].classList.remove("show");
+  // text[currentIndex].classList.remove("show");
 
   currentIndex ++;
   if(currentIndex >= totalImage){
     currentIndex = 0;
   }
   imgs[currentIndex].classList.add("show");
-  text[currentIndex].classList.add("show");
+  // text[currentIndex].classList.add("show");
 
   PageNumber();  // 페이지 넘버 버튼 상태 변경
 }
