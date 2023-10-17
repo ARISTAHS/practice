13d9d1409d0726213d7170eb9e2b500e50d73c0c
JHS <118969653+ARISTAHS@users.noreply.github.com>
2023-10-17 19:12:29 +0900

    Aboutdate 재 작업
    custom hook 사용 및 css 작업 개시

--- src/components/AboutDate.jsx
+++ src/components/AboutDate.jsx
@@ -1,27 +1,50 @@
 import React from "react";
-import { FaAnglesDown } from "react-icons/fa6";
+import '../style/About.scss'
+import { GiFencer } from "react-icons/gi";
+import { FaClipboard, FaAddressCard, FaGithub } from "react-icons/fa6";
 
 function Content({ isText }) {
   return (
     <div>{isText ? 
       (
-      <div>
-        <div class="name">JEONG HO SEOK</div><dl>
-          <dt><i class="fa-solid fa-robot"></i>CONTACT</dt>
+      <div className="detailProfile">
+        <div class="name">JEONG HO SEOK</div>
+        <dl>
+          <dt><GiFencer/>CONTACT</dt>
           <dd>010-9120-1013 // paran0907@naver.com</dd>
-        </dl><dl>
-            <dt><i class="fa-solid fa-robot"></i>SUPPORT PART</dt>
-            <dd>Web Publisher & Front-end Developer</dd>
-          </dl><dl>
-            <dt><i class="fa-solid fa-robot"></i>SKILL</dt>
-            <dd>
-              <p>HTML5, CSS3(SCSS), JavaScript, jQuery, 비동기처리(AJAX/JSON), <br />
-              반응형 Framework(Bootstrap), CMS(WordPress), SPA(VUE), Github, Photoshop
-              </p>
-            </dd>
-          </dl>
+        </dl>
+        <dl>
+          <dt><GiFencer/>SUPPORT PART</dt>
+          <dd>Web Publisher & Front-end Developer</dd>
+        </dl>
+        <dl>
+          <dt><GiFencer/>SKILL</dt>
+          <dd>
+            <p>HTML5, CSS3(SCSS), JavaScript, jQuery, 비동기처리(AJAX/JSON), <br />
+            반응형 Framework(Bootstrap), CMS(WordPress), SPA(VUE), Github, Photoshop
+            </p>
+          </dd>
+        </dl>
+
+        <ul class="popol">
+						<li>
+							<a target="_blank" href="#">스토리보드<FaClipboard /></a>
+						</li>
+						<li>
+							<a target="_blank" href="#">온라인이력서<FaAddressCard /></a>
+						</li>
+						<li>
+							<a target="_blank" href="https://github.com/ARISTAHS/www.git">GitHub<FaGithub /></a>
+						</li>
+					</ul>
+
       </div>) 
-      : '간단한 내용'}
+      : (
+        <div>
+          <p>안녕하세요, 정호석입니다. <br/>
+          <strong>자세히 보기</strong>를 클릭해주세요!</p>
+        </div>
+      )}
     </div> 
   );
 }
--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -1,22 +1,29 @@
 import React, {useState} from 'react';
-import '../style/About.scss';
-import { FaAnglesDown } from "react-icons/fa6";
+// import '../style/About.scss';
+// import { FaAnglesDown } from "react-icons/fa6";
 import Content from '../components/AboutDate';
 
 function About(){
 
 	const [show, setShow] = useState(false);
 
-	// 클릭 이벤트
+	// profile 간단히 보기와 자세히 보기에 각각 다른 css를 적용해야해서 Custom Hook 사용
+	function useProfileClass(show) {
+		return show ? 'profile' : 'profile profile-left';
+	}
+
+	// 클릭하면 숨겨진 내용 오픈
 	const clickButton = () => {
 		setShow(!show);
 	}
+
+	const profileClass = useProfileClass(show);
 
   return (
     <section id="section2">
 			<h3 className="hidden">Profile</h3>
 			
-			<div>
+			<div className={profileClass}>
 				<div className="imgContent">
 					{show ? (<img src="" alt="Image 2" />) : (<img src="" alt="Image 1" />)}
 				</div>
@@ -26,7 +33,16 @@ function About(){
 					{/* 상세 내용, 간단한 내용을 별도 컴포넌트에서 불러오기 */}
 				</div>
 				<button onClick={clickButton}>
-					{show ? '간단히 보기' : '자세히 보기'}
+					{show ? (
+						<div> 
+							<span>간단히 보기</span>
+						</div>
+						) : 
+						(
+						<div>
+							<span>자세히 보기</span>
+						</div>
+						)}
 				</button>
 			</div>
 
