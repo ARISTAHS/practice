09c7c8732b703e56a7fe37fe319ed3b43fb09d0c
JHS <118969653+ARISTAHS@users.noreply.github.com>
2023-12-16 22:02:57 +0900

    Home/About 부분 수정
    home 첫 타이핑 효과 부분 수정 예정

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -7,10 +7,10 @@ function Home(){
     <section id='section1'>
       <h3 className='hidden'>Home</h3>
       <div className='home'>
-        <TypeIt><p>멈추지 않고 <strong>저의 길을 나아가는</strong> <br />
+        <TypeIt options={{ cursor: false }}><p>멈추지 않고 <strong>저의 길을 나아가는</strong> <br />
         프론트 엔드 & 웹 퍼블리셔 <strong>정호석</strong>입니다.</p></TypeIt>
       </div>
-      <a href="#" className='scrollDown'> SCROLL DOWN <FaAnglesDown className='downIcon'/></a>
+      <a href="#section2" className='scrollDown'> SCROLL DOWN <FaAnglesDown className='downIcon'/></a>
     </section>
   );
 }
